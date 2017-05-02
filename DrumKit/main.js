13dd86e74a9fb9a91cc8d42834d9e498be7c021f
sigmafsbot <sigmacabre@gmail.com>
2017-05-02 20:29:22 +0300

    Drumkit completed v1

--- DrumKit/main.js
+++ DrumKit/main.js
@@ -7,16 +7,24 @@ function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
+
+    // function stopLag(){ let playing = false; ...};
+
+    if (e.shiftKey) {
+        audio.play();
+    } else {
+        audio.currentTime = 0;
+        audio.play();
+    }
 
     key.classList.add("playing")
 }
 
 function removeTransition(e) {
     console.log(e);
-    if (e.propertyName !== 'transform') return; // пропускаем если евент не transition
     this.classList.remove("playing");
+    if (e.propertyName !== 'transform') return; // пропускаем если евент не transition (но при этом если зажать надолго клавишу, класс перестанет убираться)
+    // this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll('.key');
